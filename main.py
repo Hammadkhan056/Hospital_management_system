f97edbf569cfa10bb58b2c5c969d3f9d27b0e580
hammadkhan056 <azamhammad783@gmail.com>
2025-10-23 15:19:50 +0500

    Let's get started

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-# Main file 
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+books = [
+    {"id": 1, "title": "1984","author": "George Orwell"},
+    {"id": 2, "title": "To kill a Mockingbird","author":"Harper Lee"}
+]
+
+@app.route('/books', methods=['GET'])
+def get_books(): 
+    return jsonify(books)
+
+@app.route('/books/<int:book_id>', methods=['GET'])
+def add_book(): 
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify(new_book),201
+
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    updated_data = request.get_json()
+    for book in books: 
+        if book["id"] == book_id:
+            book.update(updated_data)
+            return jsonify(book),404
+        
+        
+@app.route('/books/<int:book_id>',methods=['DELETE'])
+def delete_books(book_id): 
+    global books
+    books = [b for b in books if b['id'] != book_id]
+    return ("Book deleted", 204)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404 - Page Not Found</h1><p>Check your URL and try again.</p>", 404
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
--- patients.py
+++ patients.py
@@ -13,10 +13,8 @@ class patient:
         self.medical_history = None
     
     def enter__new_patient_details(self,name,age,gender,contact,address,symptoms,registered_date,medical_history): 
-        self.name = input("Enter a patient full name: ")
-        self.age = int(input("Enter a patient age: "))
-        self.gender = input("Enter a patient gender  (male/female): ")
-        self.contact = int(input("Enter  a contact number: "))
-        self.address = input("Enter a patient address: ")
-        self.symptoms = input("Enter a ")
+        while True: 
+            patient_name = input("Enter a patient name: ")
+            if patient_name: 
+                pass
         
